b519668e894dcfc23b1762b259d8e7d123a4a86e
Ace-CoLd-HoOdY <max.gongalo@gmail.com>
2025-04-22 15:05:46 +0300

    Starred transaction.java

---
+++ Transaction.java
@@ -0,0 +1,23 @@
+import scanner.util;
+
+public class Transaction{
+    string type;
+    double amount;
+    java.time.LocalTime timestamp;
+    New Scanner scanner = scanner;
+
+    this.type = type;
+    this.amount = amount;
+    this.timestamp = timestamp;
+
+
+    Void Transaction{
+        System.out.println("What transaction do you want to complete?")
+        System.out.println("Press 1 to put money on your account")
+        System.out.println("Press 2 to take money out")
+
+
+        if scanner == 1{
+            
+        }
+    }
